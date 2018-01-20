d1463ad2319fdc06073a49d34af9f9c39fec64e9
Georgi Kostadinov <georgi.d.kostadinov@abv.bg>
2018-01-20 12:47:48 +0200

    add new branch

--- app/assets/script/script.js
+++ app/assets/script/script.js
@@ -11,29 +11,28 @@
 
     document.getElementById('my-health').innerHTML += health + '<br/>'; //prints the value
 
-    $("#pushThisButton").click(function () {
+    $("#myClear").click(function () {
         document.getElementById('my-damage').innerHTML += damage; //prints the value
     });
 
     $("#pushThisButton").click(function () {
-<<<<<<< HEAD
       health = health - damage
   document.getElementById('my-output').innerHTML += health; //prints the value
       if(health < 0){
         alert("GAME OVER, YOU LOSE !");
+        health = 100;
       }
-=======
-        health = health - damage
-        document.getElementById('my-output').innerHTML += health; //prints the value
-
->>>>>>> 83b23b2215d7b56f1574be5787ff44cff6f3f222
     });
 
     /*$("#useMedKit").click(function () {
         document.getElementById('my-heal').innerHTML += health + medKit; //prints the value
     });*/
-
-
+    //  $("#myClear").click(function clearText(){
+    //  document.getElementById('output').value = "";
+    //  };
+    $("#myClear").click(function clearText(){
+     document.getElementById('myClear').value = "";
+});
 
 
 
