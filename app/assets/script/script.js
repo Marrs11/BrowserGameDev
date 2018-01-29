b0bc6164d863dec2dbed16673a3fa7a391311603
Mario Zharkov <mario.zharkov@gmail.com>
2018-01-29 20:54:11 +0200

    Added working progress bar for the HP system with bootstrap

--- app/assets/script/script.js
+++ app/assets/script/script.js
@@ -1,37 +1,26 @@
-(function (global, $) {
-    var newHealth;
-    var health;
-    health = 100;
 
-    var damage;
-    damage = 40;
+    var health = 100;
+    var damageTaken = 10;
+    var selfHealPoints = 10;
+    var medKitsAvailable = 5;
 
-    /*var medKit;
-    medKit = 30;*/
+        function attackHealth() {
+            health = health - damageTaken;
+            document.getElementById("healthId").setAttribute("style", "width:" + health + "%");
+            if (health <= 0){
+                alert("You died.");
+            }
+        }
 
-    document.getElementById('my-health').innerHTML += health + '<br/>'; //prints the value
+        function healHealth() {
+            health = health + selfHealPoints;
+            document.getElementById("healthId").setAttribute("style", "width:" + health + "%");
+            if (health > 100){
 
-    $("#pushThisButton").click(function () {
-        document.getElementById('my-damage').innerHTML += damage; //prints the value
-    });
-
-    $("#pushThisButton").click(function () {
-
-      health = health - damage
-  document.getElementById('my-output').innerHTML += health; //prints the value
-      if(health < 0){
-        alert("GAME OVER, YOU LOSE !");
-      }
-
-    });
-
-    /*$("#useMedKit").click(function () {
-        document.getElementById('my-heal').innerHTML += health + medKit; //prints the value
-    });*/
+                alert("Can't over heal");
+                document.getElementById("healHealth").disabled = true;
+            }
+        }
+        
 
 
-
-
-
-
-}(window, jQuery));
